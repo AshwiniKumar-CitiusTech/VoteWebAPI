c7840fcaaeada13ac2ee31af70cbf8517a2af1af
Ashwini Kumar <Ashwini.Kumar@wellsky.com>
2025-04-29 03:17:34 +0530

    adding cloudql configs

--- VoteService/obj/Debug/net8.0/VoteService.AssemblyInfo.cs
+++ VoteService/obj/Debug/net8.0/VoteService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VoteService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03684b06fb09321f49c3154041976399c48828bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f828a1951d9d974dfea587b66569bd011a789464")]
 [assembly: System.Reflection.AssemblyProductAttribute("VoteService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VoteService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
